a2b35aa63fe5ad686d6206dc01cb1a295efdb003
GabeHD94 <gabehd94@gmail.com>
2021-03-10 16:48:52 -0700

    sidebar of door/window sensor is refactored. Background color is now apart of the ASDoorWindow.css file

--- src/components/pages/Sensors/ASDoorWindow/ASDoorWindow.js
+++ src/components/pages/Sensors/ASDoorWindow/ASDoorWindow.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import DWAddSensor from '../ASDoorWindow/DWAddSensor/DWAddSensor'
 import ASDoorWindowSidebar from '../ASDoorWindow/ASDoorWindowSidebar'
+import {useSpring, animated} from 'react-spring'
+
 
 import './ASDoorWindow.css'
 
@@ -9,19 +11,31 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function ASDoorWindow() {
+
+    const props = useSpring({
+        from: { opacity: 0, transform: "translateX(-50%)" },
+        to: { opacity: 1, transform: "translateX(0%)" },
+        leave: { opacity: 0, transform: "translateX(50%)" },
+    })
+
     return (
-        <div className="alder-door-window-wrapper">
-             <Router>
+    <animated.div style={props}>
+        <div className="alder-door-window-container">
+
+            <div className="sidebar-space">
             <ASDoorWindowSidebar />
+            </div>
+
+            <div className="alder-door-window-wrapper">
+             <Router>
                 <Switch>
-
                     <Route path='/dw-sensor/add-sensor' component={DWAddSensor}/>
-
                     <p>Alder Door/Window Sensor</p>
                 </Switch>
-
             </Router>
         </div>
+        </div>
+        </animated.div>
     )
 }
 
--- src/components/pages/Sensors/ASDoorWindow/ASDoorWindowSidebar.js
+++ src/components/pages/Sensors/ASDoorWindow/ASDoorWindowSidebar.js
@@ -13,51 +13,51 @@ function ASDoorWindowSidebar() {
 
     return (            
     <animated.div style={props}>
-        <div className='dw-sidebar-wrapper'>
-            <h1>Door/Window Sensor</h1>
-            <ul className='dw-bars'>
-                <h2>Basic Usage</h2>
-                <Link to='/dw-sensor/add-sensor' className='dw-bars' >
-                    <li>
-                        Add New Sensor
-                    </li>
-                </Link>
+        <div className="dw-sidebar-container">
+            <div className='dw-sidebar-wrapper'>
+                <h1>Door/Window Sensor</h1>
+                <ul className='dw-bars'>
+                    <h2>Basic Usage</h2>
+                    <Link to='/dw-sensor/add-sensor' className='dw-bars' >
+                        <li>
+                            Add New Sensor
+                        </li>
+                    </Link>
 
-                <Link to='/dw-sensor/trouble-beeps' className='dw-bars' >
-                    <li>
-                        Low Battery
-                    </li>
-                </Link>
-                
-                <Link to='/dw-sensor/emergency-buttons' className='dw-bars' >
-                    <li>
-                        Tamper
-                    </li>
-                </Link>
-                <Link to='/dw-sensor/add-user-codes' className='dw-bars' >
-                    <li>
-                        Loss of Supervision
-                    </li>
-                </Link>
-                <Link to='/dw-sensor/change-remove-user-codes' className='dw-bars' >
-                    <li>
-                        False Alarm
-                    </li>
-                </Link>
-                <Link to='/dw-sensor/brightness-timeout' className='dw-bars' >
-                    <li>
-                        Test/Bypass
-                    </li>
-                </Link>
-                <Link to='/dw-sensor/clean-screen' className='dw-bars' >  
-                    <li>
-                        Battery Type
-                    </li>
-                </Link>   
+                    <Link to='/dw-sensor/trouble-beeps' className='dw-bars' >
+                        <li>
+                            Low Battery
+                        </li>
+                    </Link>
                     
-            </ul>
-            &nbsp;
-
+                    <Link to='/dw-sensor/emergency-buttons' className='dw-bars' >
+                        <li>
+                            Tamper
+                        </li>
+                    </Link>
+                    <Link to='/dw-sensor/add-user-codes' className='dw-bars' >
+                        <li>
+                            Loss of Supervision
+                        </li>
+                    </Link>
+                    <Link to='/dw-sensor/change-remove-user-codes' className='dw-bars' >
+                        <li>
+                            False Alarm
+                        </li>
+                    </Link>
+                    <Link to='/dw-sensor/brightness-timeout' className='dw-bars' >
+                        <li>
+                            Test/Bypass
+                        </li>
+                    </Link>
+                    <Link to='/dw-sensor/clean-screen' className='dw-bars' >  
+                        <li>
+                            Battery Type
+                        </li>
+                    </Link>   
+                </ul>
+                &nbsp;
+            </div>
         </div>
         </animated.div>
     )
